b56179390dfe4718951b782c0e5e5eadd99635d1
Eduardo Ferreira Leite <34074116+EduardoHacksmen@users.noreply.github.com>
2017-11-28 19:14:37 -0200

    Create  1924-Vitória_e_a_Indecisão.c

---
+++ 1924-Vitória_e_a_Indecisão.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#define max 100
+int main()
+{
+    int i, n;
+    char curso[max];
+    scanf("%d", &n);
+    while(n>0){
+        scanf("%s", curso);
+        n--;
+    }
+    printf("Ciencia da Computacao\n");
+    
+    
+    
+    return 0;
+}
+
+
